4301eddf46854c1cf16704214e7273514e0ee66f
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-04-26 18:21:05 -0500

    Add erase function in list_functions.js file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import editincon from './edit_icon_img.png';
 import refreshicon from './refresh_icon_img.png';
 import entericon from './enter_icon_img.png';
 import * as fun from "./list_functions.js";
+import deleteicon from "./erase_icon_img.png";
+
 
 const refrescontainer = document.querySelector('.title img');
 const entercontainer = document.querySelector('.text-input img');
@@ -16,7 +18,7 @@ const taskarr = [
 ];
 
 const storagedtasks = () => {
-  taskarr.sort((a, b) => a.index - b.index);
+ 
   taskarr.forEach((element) => {
     taskcontainer.innerHTML += `<div class="tasks-item" id="${element.index}">
 <div class="tasks-item-start"><img class="checkboxicon" src="${checkboxicon}" alt="checkbox icon">
@@ -25,7 +27,7 @@ const storagedtasks = () => {
 </div>
 
 <img class="editicon" src="${editincon}" alt="edit icon">
-<img class="removeicon" src="" alt="remove icon">
+<img class="removeicon" src="${deleteicon}" alt="remove icon">
 </div>`;
   });
 };
@@ -42,16 +44,17 @@ inputtext.addEventListener("keypress", (event)=> {
 <input class="edit_text" type="text" placeholder="Edit Task">
 </div>
 <img class="edit_icon" src="${editincon}" alt="edit icon">
-<img class="removeicon" src="" alt="remove icon">
+<img class="removeicon" src="${deleteicon}" alt="remove icon">
 </div>`;
 
 const editcontainer=document.querySelectorAll(".tasks-item-start p")
 const editbutton=document.querySelectorAll(".edit_icon")
+const remove_icon=document.querySelectorAll(".removeicon")
 const editinput=document.querySelectorAll(".edit_text")
-const task=document.querySelector(".tasks-item")
-
 editbutton.forEach((element,index)=>{element.addEventListener("click", ()=>{
+  remove_icon[index].classList.add("active")
   editcontainer[index].classList.add("active")
+  editbutton[index].classList.add("active")
   editinput[index].classList.add("active")
   editinput[index].addEventListener("keypress",(event)=>{
     if(event.key==="Enter"&& editinput[index].value!==""){
@@ -59,11 +62,20 @@ editbutton.forEach((element,index)=>{element.addEventListener("click", ()=>{
       editcontainer[index].textContent=taskarr[index].description
       editcontainer[index].classList.remove("active")
       editinput[index].classList.remove("active")
-      console.log(taskarr)
+      editbutton[index].classList.remove("active")
+      remove_icon[index].classList.remove("active")
     }}
   )}
 )} 
-)}
+)
+remove_icon.forEach((element,index)=>{element.addEventListener("click",()=>{
+  fun.erase(taskarr,remove_icon[index].parentElement.id)
+  remove_icon[index].parentElement.remove()
+  const tasks=document.querySelectorAll(".tasks-item")
+  tasks.forEach((element,index)=>{element.id=taskarr[index].index})
+  })
+})
+}
   })
  
 
--- src/list_functions.js
+++ src/list_functions.js
@@ -12,3 +12,9 @@ export const edit=(editinput,taskarr,id)=>{
     taskarr[id].description=editinput
 }
 
+export const erase=(taskarr,id)=>{
+    taskarr.splice(id-1,1)
+    taskarr.forEach((element,index)=>{
+        element.index=index+1
+    })
+    };
