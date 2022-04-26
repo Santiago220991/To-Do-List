6653761dd22c3d1fffcf39378df8b8cf87a0f1e9
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-04-26 12:50:00 -0500

    Let taskarr empty

--- src/index.js
+++ src/index.js
@@ -9,27 +9,7 @@ const entercontainer = document.querySelector('.text-input img');
 const taskcontainer = document.querySelector('.tasks');
 
 const taskarr = [
-  {
-    description: 'Fourt Task',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Second Task',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Third Task',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'First Task',
-    completed: false,
-    index: 0,
-  },
-
+  
 ];
 
 refrescontainer.src = refreshicon;
