faabc6d098d9f32bd0cb7d5596a73045aa87881e
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-04-26 22:15:12 -0500

    Check linters ready to pull request

--- src/list_functions.js
+++ src/list_functions.js
@@ -1,20 +1,19 @@
-export const add=(inputtext,taskarr)=>{
-    let len=taskarr.length
-    taskarr.push({
-        description: inputtext,
-        completed: false,
-        index: len+1,
-    })
-    
-}
+export const add = (inputtext, taskarr) => {
+  const len = taskarr.length;
+  taskarr.push({
+    description: inputtext,
+    completed: false,
+    index: len + 1,
+  });
+};
 
-export const edit=(editinput,taskarr,id)=>{
-    taskarr[id].description=editinput
-}
+export const edit = (editinput, taskarr, id) => {
+  taskarr[id].description = editinput;
+};
 
-export const erase=(taskarr,id)=>{
-    taskarr.splice(id-1,1)
-    taskarr.forEach((element,index)=>{
-        element.index=index+1
-    })
-    };
+export const erase = (taskarr, id) => {
+  taskarr.splice(id - 1, 1);
+  taskarr.forEach((element, index) => {
+    element.index = index + 1;
+  });
+};
