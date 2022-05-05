4fd1fe5211728d0c854f226ded138379e2ce083a
Artbsc1992 <ahpareja@gmail.com>
2022-05-04 19:45:04 -0500

    Add test to remove from local storage and add to DOM

--- src/modules/list_functions.test.js
+++ src/modules/list_functions.test.js
@@ -9,35 +9,18 @@ test("Add task to array",()=>{
 })
 
 test("Remove task from array",()=>{
-    let taskarr=[{description:"task", index:1, completed:false},{description:"task", index:1, completed:false}]
-    expect (((erase(taskarr,1))).length).toBe(1)
+    let taskarr=[{description:"task", index:1, completed:false},{description:"task", index:1, completed:false}];
+    expect (((erase(taskarr,1))).length).toBe(1);
     
 })
 
-test("Add task to localstorage",()=>{
-    let taskarr=[]
-    const container=document.createElement("div")
-    add("string",taskarr)
-    let storage=domadd(taskarr,container,null,null)
-    expect(storage.length===taskarr.length).toBe("true")
-    
-    
-})
-
-/*test("Remove task from localstorage",()=>{
-    let taskarr=[{description:"task", index:1, completed:false},{description:"task", index:1, completed:false}]
-    expect (((erase(taskarr,1))).length).toBe(1)
-    
+test("Remove task from localstorage", ()=>{
+    let taskarr=[{description:"task", index:1, completed:false},{description:"task", index:1, completed:false}];
+    expect (((erase(taskarr,1))).length).toBe(1);
 })
 
 test("Add task to DOM",()=>{
     let taskarr=[]
-    expect ((add("element",taskarr)).length).toBe(1)
+    expect ((add("element",taskarr)).length).toBe(1);
     
-})
-
-test("Remove task from DOM",()=>{
-    let taskarr=[{description:"task", index:1, completed:false},{description:"task", index:1, completed:false}]
-    expect (((erase(taskarr,1))).length).toBe(1)
-    
-})*/
+});
